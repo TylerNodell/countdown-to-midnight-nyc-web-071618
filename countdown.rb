3544a6f4d60d0c072eeefea824eebf15af17c020
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-06 14:41:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(value)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(value)
+  number = value
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
