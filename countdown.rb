3f1d4042dc1bbd1b5db16a840d3060a3976b9c38
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-06 14:36:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(value)
+  number = value
+  until number == 1
+    puts "#{number} SECOND(S)!"
+    sleep(100)
+  end
+  puts "HAPPY NEW YEAR!"
 end
